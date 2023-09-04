b35b3e78bca659fcfec7f2daf8f052ea2008d4c1
santhosh <santhoshsai4517@gmail.com>
2023-09-04 11:27:23 +0530

    Completed

--- src/main/java/com/ecommerce/EProduct.java
+++ src/main/java/com/ecommerce/EProduct.java
@@ -3,22 +3,29 @@ package com.ecommerce;
 import java.math.BigDecimal;
 import java.util.Date;
 
-public class EProduct {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;  
+import javax.persistence.Table;  
+  
+@Entity  
+@Table(name= "eproduct")   
+public class EProduct {    
+  
+        @Id @GeneratedValue   
+        @Column(name = "ID")
         private long ID;
+        
+        @Column(name = "name")
         private String name;
+        
+        @Column(name = "price")
         private BigDecimal price;
-        private Date dateAdded;
         
-        public EProduct() {
-                
-        }
-        public EProduct(long id, String name, BigDecimal price, Date dateAdded) {
-                this.ID = id;
-                this.name = name;
-                this.price = price;
-                this.dateAdded = dateAdded;
-        }
-        
+        @Column(name = "date_added")
+        private Date dateAdded;  
+            
         public long getID() {return this.ID; }
         public String getName() { return this.name;}
         public BigDecimal getPrice() { return this.price;}
@@ -27,7 +34,5 @@ public class EProduct {
         public void setID(long id) { this.ID = id;}
         public void setName(String name) { this.name = name;}
         public void setPrice(BigDecimal price) { this.price = price;}
-        public void setDateAdded(Date date) { this.dateAdded = date;}
-        
-        
-}
+        public void setDateAdded(Date date) { this.dateAdded = date;}    
+}   
